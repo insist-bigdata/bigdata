394f77dd0912b3ab53e6e20d2476d5beb235cae6
damoncai <782099197@qq.com>
2021-11-05 09:51:58 +0800

    azkaban

---
+++ bigdata_06_azkaban/src/main/java/top/damoncai/azkaban/javaprocess/Test.java
@@ -0,0 +1,13 @@
+package top.damoncai.azkaban.javaprocess;
+
+/**
+ * @author zhishun.cai
+ * @date 2021/11/4 16:02
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("JavaProcess Test~~~");
+    }
+}
